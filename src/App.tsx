3dfef98b136a88292c5378bd2dff04b7f1decb31
pmqueiroz <pedroqueiroz593@gmail.com>
2020-08-03 23:03:40 -0300

    Fix: Build Problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './assets/styles/global.css';
 
-import Landing from './pages/Landing'
 import Routes from './routes';
 
 function App() {
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
