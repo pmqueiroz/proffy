4122a950ea6b3e0112591b7ab5929633e0f26c2d
pmqueiroz <pedroqueiroz593@gmail.com>
2020-08-04 11:00:38 -0300

    Add: Api Rest

---
+++ server/src/utils/convertHoursToMinutes.ts
@@ -0,0 +1,7 @@
+export default function convertHourToMinutes(time: string){
+    const [hour, minutes] = time.split(':').map(Number);
+    const timeInMinutes = (hour * 60) + minutes;
+    
+    return timeInMinutes;
+
+}
