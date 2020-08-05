4fc0b89bda26a978fd9c5cc2d7b8ab8c6ecffd91
pmqueiroz <pedroqueiroz593@gmail.com>
2020-08-05 09:59:04 -0300

    Web: Last Commit

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 import warningIcon from '../../assets/images/icons/warning.svg'
 
@@ -20,13 +22,56 @@ function TeacherForm(){
                     <Input name="name" label="Complete Name" />
                     <Input name="avatar" label="Avatar" />
                     <Input name="whatsapp" label="Whatsapp" />
+                    <Textarea name="bio" label="Biography"/>
                 </fieldset>
 
                 <fieldset>
                     <legend>About your lesson</legend>
                     
-                    <Input name="subject" label="Subject" />
+                    <Select 
+                        name="subject" 
+                        label="Subject" 
+                        options={[
+                            { value: 'Literature', label: 'Literature'},
+                            { value: 'Speech', label: 'Speech'},
+                            { value: 'Writing or Composition', label: 'Writing or Composition'},
+                            { value: 'Algebra', label: 'Algebra'},
+                            { value: 'Algebra 2', label: 'Algebra II'},
+                            { value: 'Geometry', label: 'Geometry'},
+                            { value: 'World History', label: 'World History'},
+                            { value: 'Spanish', label: 'Spanish'},
+                            { value: 'German', label: 'German'},
+                            { value: 'French', label: 'French'},
+                            { value: 'Chemistry', label: 'Chemistry'},
+                            { value: 'Physics', label: 'Physics'},
+                        ]}
+                    />
                     <Input name="cost" label="Hour cost for your class" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>Available Times
+                        <button type="button">
+                            + New time
+                        </button>
+                    </legend>
+                    <div className="schedule-item">
+                        <Select 
+                            name="weed_day" 
+                            label="Week Day" 
+                            options={[
+                                { value: '0', label: 'Sunday'},
+                                { value: '1', label: 'Monday'},
+                                { value: '2', label: 'Tuesday'},
+                                { value: '3', label: 'Wednesday'},
+                                { value: '4', label: 'Thursday'},
+                                { value: '5', label: 'Friday'},
+                                { value: '6', label: 'Saturday'},
+                            ]}
+                        />
+                        <Input name="from" label="From" type="time"/>
+                        <Input name="to" label="to" type="time"/>
+                    </div>
                 </fieldset>
 
                 <footer>
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import './style.css'
 
@@ -11,8 +12,37 @@ function TeacherList(){
         <div id="page-teacher-list" className="container">
             <PageHeader title="These are the available proffys.">
                 <form id="search-teachers">
-                    <Input name="subject" label="Subject"/>
-                    <Input name="week-day" label="Week Day"/>
+                    <Select 
+                        name="subject" 
+                        label="Subject" 
+                        options={[
+                            { value: 'Literature', label: 'Literature'},
+                            { value: 'Speech', label: 'Speech'},
+                            { value: 'Writing or Composition', label: 'Writing or Composition'},
+                            { value: 'Algebra', label: 'Algebra'},
+                            { value: 'Algebra 2', label: 'Algebra II'},
+                            { value: 'Geometry', label: 'Geometry'},
+                            { value: 'World History', label: 'World History'},
+                            { value: 'Spanish', label: 'Spanish'},
+                            { value: 'German', label: 'German'},
+                            { value: 'French', label: 'French'},
+                            { value: 'Chemistry', label: 'Chemistry'},
+                            { value: 'Physics', label: 'Physics'},
+                        ]}
+                    />
+                    <Select 
+                        name="weed_day" 
+                        label="Week Day" 
+                        options={[
+                            { value: '0', label: 'Sunday'},
+                            { value: '1', label: 'Monday'},
+                            { value: '2', label: 'Tuesday'},
+                            { value: '3', label: 'Wednesday'},
+                            { value: '4', label: 'Thursday'},
+                            { value: '5', label: 'Friday'},
+                            { value: '6', label: 'Saturday'},
+                        ]}
+                    />
                     <Input type="time" name="time" label="Hour"/>
                 </form>
             </PageHeader>
